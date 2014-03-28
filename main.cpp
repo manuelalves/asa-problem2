656d46d41ec40d9b2838a3f3bec3141b15c616a3
manuelalves <manuelalves@tecnico.ulisboa.pt>
2014-03-28 15:34:55 +0000

    create main.cpp file

---
+++ main.cpp
@@ -0,0 +1,24 @@
+/*
+
+Grupo 108
+
+70179 - Manuel Alves     [manuelalves@tecnico.ulisboa.pt]
+70298 - Jessica Ribeiro  [jessica.ribeiro@tecnico.ulisboa.pt]
+
+*/
+
+#include <iostream>
+#include <list>
+#include <vector>
+#include <algorithm>
+#include <stack>
+#include <stdio.h>
+
+using namespace std;
+
+
+
+int main(){
+
+       cout << "\n";
+}
